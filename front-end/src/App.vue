b0024eaf99d8856950666982da7f2312f2125847
joeparis6 <78243702+joeparis6@users.noreply.github.com>
2021-12-13 18:32:53 -0700

    first

---
+++ front-end/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+<div id="app">
+  <div class="header">
+    <router-link to="/">
+      <div class="logo">
+        <p>Home </p>
+        <p>     </p>
+      </div>
+    </router-link>
+    <router-link to="/admin">
+      <div class="logo">
+        <p>   Admin</p>
+
+
+      </div>
+    </router-link>
+    <div class="title">
+      <h1>Basketball Players</h1>
+    </div>
+  </div>
+  <div class="content">
+    <router-view />
+  </div>
+  <div class="footer">
+    <router-link to="/admin">Admin</router-link>
+  </div>
+</div>
+</template>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+
+body {
+background: #2222ed;
+  text-align: center;
+  color: white;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 16px;
+  padding: 0px;
+  margin: 0px;
+}
+
+/* Header */
+.header {
+  display: flex;
+  padding: 10px 100px 0px 100px;
+  background-color: #FFA500;
+  color: #1C454F;
+}
+
+.title {
+  margin-top: 5px;
+}
+
+.title h1 {
+  text-align: center;
+  font-size: 30px;
+}
+
+.content {
+  padding: 20px 100px;
+  min-height: 500px;
+}
+
+.logo {
+  margin: 10px;
+}
+
+/* Footer */
+.footer {
+  height: 50px;
+  padding: 20px 100px 0px 100px;
+  background: #e3e3e3;
+  font-size: 12px;
+}
+
+.footer a {
+  color: #000;
+}
+
+h1 {
+  font-size: 20px;
+}
+
+h2 {
+  font-size: 14px;
+}
+</style>
---
+++ front-end/src/views/Admin.vue
@@ -0,0 +1,223 @@
+<template>
+<div class="admin">
+  <h1>Add/Delete Players</h1>
+  <div class="heading">
+    <div class="circle">1</div>
+    <h2>Add a Player</h2>
+  </div>
+  <div class="add">
+    <div class="form">
+      <input v-model="player" placeholder="Name">
+      <p></p>
+      <textarea v-model="college" placeholder="College"></textarea>
+      <p></p>
+      <input type="file" name="photo" @change="fileChanged">
+      <button @click="upload">Upload</button>
+    </div>
+    <div class="upload" v-if="addItem">
+      <h2>{{addItem.player}}</h2>
+      <h2>{{addItem.college}}</h2>
+      <img :src="addItem.path" />
+
+    </div>
+  </div>
+
+
+<div class="heading">
+<div class="circle">2</div>
+<h2>Edit/Delete a Player</h2>
+</div>
+<div class="edit">
+<div class="form">
+  <input v-model="findTitle" placeholder="Search">
+  <div class="suggestions" v-if="suggestions.length > 0">
+    <div class="suggestion" v-for="s in suggestions" :key="s.id" @click="selectItem(s)">{{s.player}}
+    </div>
+  </div>
+</div>
+<div class="upload" v-if="findItem">
+  <input v-model="findItem.player">
+  <textarea v-model="findItem.college"></textarea>
+  <p></p>
+  <img :src="findItem.path" />
+</div>
+<div class="actions" v-if="findItem">
+  <button @click="deleteItem(findItem)">Delete</button>
+  <button @click="editItem(findItem)">Edit</button>
+
+</div>
+</div>
+
+<footer>
+
+</footer>
+
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Admin',
+  data() {
+  return {
+    player: "",
+    college: "",
+    file: null,
+    addItem: null,
+    items: [],
+    findTitle: "",
+    findItem: null,
+  }
+},
+computed: {
+  suggestions() {
+    let items = this.items.filter(item => item.player.toLowerCase().startsWith(this.findTitle.toLowerCase()));
+    return items.sort((a, b) => a.player > b.player);
+  }
+},
+
+created() {
+  this.getItems();
+},
+
+  methods: {
+    fileChanged(event) {
+      this.file = event.target.files[0]
+    },
+    async upload() {
+      try {
+        const formData = new FormData();
+        formData.append('photo', this.file, this.file.name)
+        let r1 = await axios.post('/api/photos', formData);
+        let r2 = await axios.post('/api/items', {
+          player: this.player,
+          college: this.college,
+          path: r1.data.path
+        });
+        this.addItem = r2.data;
+      } catch (error) {
+        // error
+      }
+    },
+    async getItems() {
+      try {
+        let response = await axios.get("/api/items");
+        this.items = response.data;
+
+        return true;
+      } catch (error) {
+        // error
+      }
+    },
+    selectItem(item) {
+      this.findTitle = "";
+      this.findItem = item;
+    },
+    async deleteItem(item) {
+      try {
+        await axios.delete("/api/items/" + item._id);
+        this.findItem = null;
+        this.getItems();
+        return true;
+      } catch (error) {
+        // error
+      }
+    },
+    async editItem(item) {
+      try {
+        await axios.put("/api/items/" + item._id, {
+          player: this.findItem.player,
+          college: this.findItem.college,
+        });
+        this.findItem = null;
+        this.getItems();
+        return true;
+      } catch (error) {
+        // error
+      }
+    },
+
+
+  }
+
+}
+
+
+</script>
+
+
+<style scoped>
+.image h2 {
+  font-style: italic;
+  font-size: 1em;
+}
+
+.heading {
+  display: flex;
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
+
+.heading h2 h3 {
+  margin-top: 8px;
+  margin-left: 10px;
+}
+
+.add,
+.edit {
+  display: flex;
+}
+
+.circle {
+  border-radius: 50%;
+  width: 18px;
+  height: 18px;
+  padding: 8px;
+  background: #333;
+  color: #fff;
+  text-align: center
+}
+
+/* Form */
+input,
+textarea,
+select,
+button {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1em;
+}
+
+.form {
+  margin-left: 250px;
+  margin-right: 100px;
+  padding: 12px;
+}
+
+/* Uploaded images */
+.upload h2 {
+  margin: 0px;
+}
+
+.upload img {
+  max-width: 300px;
+}
+
+/* Suggestions */
+.suggestions {
+  width: 200px;
+  border: 1px solid #ccc;
+}
+
+.suggestion {
+  min-height: 20px;
+}
+
+.suggestion:hover {
+  background-color: #5BDEFF;
+  color: #fff;
+}
+
+
+</style>
